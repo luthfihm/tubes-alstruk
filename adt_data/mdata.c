628c879492c292c54fd629160618fafe73e0a593
fahziar <fahziar_rw@yahoo.co.uk>
2013-12-02 13:55:19 +0700

    Update driver mdata.c

--- adt_data/mdata.c
+++ adt_data/mdata.c
@@ -6,14 +6,18 @@ int main()
 	//Kamus
 	tabData data;
 	String date;
+	String newNama, delNama;
 	int i;
 	
 	//Program
-
+	
+	printf("Loading data from file...\n");
 	
 	if (!loadData(&data))
 	{
+		printf("Load data succesfull..\nSorting data by its score..\n");
 		sortScoreDesc(&data);
+		printf("Data setelah di sort:\n");
 		for (i = 1; i <= data.jumlahPemain; i++)
 		{
 			printf("Nama: %s\n", data.TI[i].nama);
@@ -22,11 +26,42 @@ int main()
 			intToWaktu(data.TI[i].time, date);
 			printf("Last played: %s\n\n", date);
 		}
-		addPengguna(&data, "Oji", 75, time(NULL));
-		printf ("%d\n", delPengguna(&data, "Oji"));
+		
+		printf("Masukkan nama pengguna untuk ditambahkan ke data:\n");
+		scanf("%s", newNama);
+		if(IsMemberData(data, newNama))
+		{
+			printf("Nama pengguna sudah ada di data\n");
+		} else {
+			if(IsTabFull(data))
+			{
+				printf("Tabel penuh\n");
+			} else {
+				addPengguna(&data, newNama, 75, time(NULL));
+				printf("Pengguna berhasil ditambahkan ke tabel data\n");
+			}
+		}
+		
+		printf("Masukkan nama pengguna untuk dihapus dari tabel:\n");
+		scanf("%s", delNama);
+		if(IsMemberData(data, delNama))
+		{
+			if(!delPengguna(&data, delNama))
+			{
+				printf("Pengguna gagal dihapus dari data\n");
+			} else {
+				printf("Pengguna berhasil dihapus dari data\n");
+			}
+		} else {
+			printf("Nama yang anda masukkan tidak ada dalam tabel\n");
+		}
+		
+		printf("Menyimpan data ke file...\n");
 		saveData(data);
+		
+		printf("done..\n");
 	} else {
-		printf("Error\n");
+		printf("Error loading data\n");
 	}
 	
 	return 0;
